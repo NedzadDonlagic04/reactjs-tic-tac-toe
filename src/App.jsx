dc1359c19f8d6782873169b2ddc4594021ac41dc
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-03-12 23:59:38 +0100

    Imported and implemented App component props

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,13 @@
-function App() {
+import PropTypes from 'prop-types';
+
+function App(props) {
 	return (
-		<h1>Hello World!</h1>
+		<h1>{props.title}</h1>
 	);
+}
+
+App.propTypes = {
+	title: PropTypes.string.isRequired
 }
 
 export default App;
