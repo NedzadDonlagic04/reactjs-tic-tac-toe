eb46478b0c5c4fe7e884900117af4b17a006c3ad
YourFBIAgent <nedzaddonlagic04@gmail.com>
2023-03-18 10:21:22 +0100

    Added icon change option on theme preference

--- src/main.jsx
+++ src/main.jsx
@@ -8,3 +8,10 @@ ReactDOM.createRoot(document.getElementById('root')).render(
 		<App title='Tic Tac Toe'/>
 	</React.StrictMode>,
 );
+
+
+const link = document.querySelector('link[rel="icon"]');
+
+if(window.matchMedia('(prefers-color-scheme: dark)').matches) {
+    link.href = '/tic-tac-toe-icon-light.png';
+}
